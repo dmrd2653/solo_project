8c1aa5ef1733298cd9150051f18602957796ea56
Daniel Davidson <danielmcraedavidson@gmail.com>
2023-08-08 20:29:29 -0500

    styling and updated database.sql

--- database.sql
+++ database.sql
@@ -1,10 +1,21 @@
 
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
+);
+
+CREATE TABLE "travel_list" (
+	"id" integer PRIMARY KEY,
+	"name" varchar(80) NOT NULL,
+	"user_id" integer REFERENCES "user" (id)
+);
+
+CREATE TABLE "places" (
+	"id" integer PRIMARY KEY,
+	"name" varchar(80) NOT NULL,
+	"location" varchar(80) NOT NULL,
+	"category" varchar(80) NOT NULL,
+	"notes" varchar(120),
+	"list_id" integer REFERENCES "travel_list" (id)
 );
--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -13,14 +13,14 @@ function LandingPage() {
   return (
     <div className="container">
       <div className="grid">
-        <div className="grid-col grid-col_5">
+        <div className="grid-col">
           <LoginForm />
-          <center>
+        </div>
+        <div className="grid-col new-user">
             <h4>New User?</h4>
             <button className="btn btn_sizeSm" onClick={onRegister}>
               Register
             </button>
-          </center>
         </div>
       </div>
     </div>
